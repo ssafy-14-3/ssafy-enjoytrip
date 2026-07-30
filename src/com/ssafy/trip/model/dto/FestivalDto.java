3cd2547c8a7838d57d4f5094f77ef7c4ffe92741
medAndro <med4ndro@gmail.com>
2026-07-30 14:38:13 +0900

    feat: FestivalDto, Dao의 인터페이스 작성

---
+++ src/com/ssafy/trip/model/dto/FestivalDto.java
@@ -0,0 +1,74 @@
+package com.ssafy.trip.model.dto;
+
+import java.util.Objects;
+
+public class FestivalDto {
+	/** 연번 */
+	private int num;
+	/** 축제명 */
+	private String festivalName;
+	/** 시, 도 */
+	private String city;
+	/** 시작일 */
+	private String startDate;
+	/** 종료일 */
+	private String endDate;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getFestivalName() {
+		return festivalName;
+	}
+
+	public void setFestivalName(String festivalName) {
+		this.festivalName = festivalName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Integer.valueOf(num));
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FestivalDto other = (FestivalDto) obj;
+		return num == other.num;
+	}
+
+}
